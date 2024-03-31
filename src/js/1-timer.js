608b4aa0413e7b4b8238821b6c0407fb0737eb2e
Natalia-Kalashnikova <144282173+Natalia-Kalashnikova@users.noreply.github.com>
2024-03-31 07:06:49 +0300

    fix

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -59,16 +59,16 @@ function updateDisplay() {
   document.querySelector('[data-seconds]').textContent = addTimerValue(seconds);
 }
 
-function convertMs(remainingTime) {
+function convertMs(ms) {
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  const days = Math.floor(remainingTime / day);
-  const hours = Math.floor((remainingTime % day) / hour);
-  const minutes = Math.floor(((remainingTime % day) % hour) / minute);
-  const seconds = Math.floor((((remainingTime % day) % hour) % minute) / second);
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
 }
